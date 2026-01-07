400ff29761be9cf82d61cc31e7bb2f25b6e72522
shynur <shynur@outlook.com>
2026-01-07 17:02:01 +0800

    restruct cmake arch

--- src/cmake/QuickJSConfig.cmake
+++ src/cmake/QuickJSConfig.cmake
@@ -1,4 +1,5 @@
 include(${CMAKE_CURRENT_LIST_DIR}/QuickJSTargets.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/QuickJSXXTargets.cmake)
 
 include(CMakeFindDependencyMacro)
 #find_dependency(m)
--- src/quickjsxx/quickjsxx.hpp
+++ src/quickjsxx/quickjsxx.hpp
@@ -1,1 +1,9 @@
 #pragma once
+#include <string>
+
+using namespace std::literals;
+
+namespace quickjsxx {
+
+
+}
